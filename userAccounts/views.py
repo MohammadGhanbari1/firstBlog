3b9277c2998aa6c1f3c945c3b795a16f9b3cac7e
mohammad-ghanbari <mohammad.ghanbari.pc@gmail.com>
2020-12-25 23:25:34 +0330

    new
    Co-Authored-By: Mohammad Ghanbari <MohammadGhanbari1@users.noreply.github.com>

---
+++ userAccounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout
+# Create your views here.
+
+def signup_views(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            # login
+
+            return redirect('articles:list')
+    else:
+            form = UserCreationForm()
+    return render(request,'userAccounts/signup.html',{'form':form})
+
+def login_views(request):
+    if request.method=='POST':
+        form=AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user=form.get_user()
+
+            login(request,user)
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect('articles:list')
+    else:
+        form=AuthenticationForm()
+    return  render(request,'userAccounts/login.html',{'form':form})
+
+def logout_views(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
